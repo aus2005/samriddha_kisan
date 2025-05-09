dfbd246e0ae1f54ebba0bf96808a28c52947ca9a
Austina Aryal <aryalaustina771@gmail.com>
2025-05-09 18:27:11 +0545

    .

--- myproject/crop_monitoring/forms.py
+++ myproject/crop_monitoring/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class ImageUploadForm(forms.Form):
-    image = forms.ImageField(label="बिरुवाको तस्वीर अपलोड गर्नुहोस्")
+    image = forms.ImageField(label="तस्वीर अपलोड")
