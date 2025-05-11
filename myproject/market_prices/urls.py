b03c598f07a71c1f6468be21ca0f9abdf6ae7f31
Austina Aryal <aryalaustina771@gmail.com>
2025-05-11 15:57:55 +0545

    prices

--- myproject/market_prices/urls.py
+++ myproject/market_prices/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.placeholder, name='placeholder'),
+    path('', views.today_table, name='today_table'),
+    path('update/', views.update_prices, name='update_prices'),
 ]
--- myproject/market_prices/views.py
+++ myproject/market_prices/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import PriceTable
+from .utils import scrape_and_save_table  
+from datetime import date
 
-def placeholder(request):
-    return HttpResponse("Coming Soon")
+def today_table(request):
+    try:
+        price_table = PriceTable.objects.get(date=date.today())
+        table_html = price_table.html
+    except PriceTable.DoesNotExist:
+        table_html = "<p>आजको लागि तालिका उपलब्ध छैन।</p>"
+
+    return render(request, "market_prices/today_table.html", {"table_html": table_html})
+
+def update_prices(request):
+    if request.method == "POST":
+        scrape_and_save_table()
+    return redirect("today_table")
