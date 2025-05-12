140a6f2f9da289af88bfdf84f78573583c2c6d5d
Austina Aryal <aryalaustina771@gmail.com>
2025-05-12 13:38:51 +0545

    prices fix

--- myproject/users/models.py
+++ myproject/users/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    ROLE_CHOICES = (
+        ('farmer', 'किसान'),
+        ('buyer', 'खरिदकर्ता'),
+    )
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.get_role_display()}"
+
+# Signal to create or update user profile when user is saved
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
